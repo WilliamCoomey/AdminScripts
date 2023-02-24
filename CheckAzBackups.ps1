a94ce4de84ff59ceb809537cdb78ccb90f115ac4
WilliamCoomey <william.coomey@outlook.com>
2023-02-24 13:29:28 +0000

    Updated to allow multiple valuts

--- CheckAzBackups.ps1
+++ CheckAzBackups.ps1
@@ -1,8 +1,8 @@
 ﻿#Script to output the status of Azure backups
 
 Connect-AzAccount | Out-Null
-$vault = Get-AzRecoveryServicesVault
-$output = Get-AzRecoveryServicesBackupJob -VaultId $vault.ID | Select-Object -Property WorkLoadName, Status, StartTime, EndTime | Format-Table
+$vaults = Get-AzRecoveryServicesVault
+$output = $vaults | % {Get-AzRecoveryServicesBackupJob -VaultId $_.ID} | Select-Object -Property WorkLoadName, Status, StartTime, EndTime | Format-Table
 
 #Need to pipe output to get around printing after pause statement bug
 $output | Out-Host
